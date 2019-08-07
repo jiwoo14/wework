f8e5dc71af9efe8b7537b6ed80171a326611d74d
JIWOOLEE <koljw333@gmail.com>
2019-08-07 16:00:05 +0900

    wework

---
+++ project/registration_login/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class RegistrationLoginConfig(AppConfig):
+    name = 'registration_login'
---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\koljw\OneDrive\바탕 화면\LikeLion\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
